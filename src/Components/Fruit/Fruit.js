563cb4b5c53b74e9dad0dbd7cb8846f07c0ac002
MOTIUR <motiur@MOTIURs-MacBook-Pro.local>
2022-09-29 21:40:16 +0600

    Fix button alignment issue

--- src/Components/Fruit/Fruit.js
+++ src/Components/Fruit/Fruit.js
@@ -5,21 +5,20 @@ const Fruit = (props) => {
   const { id, img, age, name, desc, time } = props.fruit;
 
   return (
-    <div id="fruit" className="col text-left p-5">
+    <div className="col text-left p-5 fruit">
       <img className="img-fluid" src={img} alt="" />
       <h4>{name}</h4>
       <p>{desc.slice(0, 150)}</p>
       <p>For Age:{age}</p>
       <p>Time Required:{time} s</p>
-      <div class="d-grid gap-2">
-        <button
-          onClick={() => props.consumedTime(time)}
-          className="btn btn-primary"
-          type="button"
-        >
-          Add to List
-        </button>
-      </div>
+
+      <button
+        onClick={() => props.consumedTime(time)}
+        className="btn btn-primary addToListbtn"
+        type="button"
+      >
+        Add to List
+      </button>
     </div>
   );
 };
